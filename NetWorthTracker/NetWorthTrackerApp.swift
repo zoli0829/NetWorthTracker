baf227f5142dc9bba9acc65e0fc4826ae6b3463c
zoli0829 <zoli0829@gmail.com>
2025-10-20 20:20:16 +0100

    Initial commit

---
+++ NetWorthTracker/NetWorthTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetWorthTrackerApp.swift
+//  NetWorthTracker
+//
+//  Created by Zoltan Vegh on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct NetWorthTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
